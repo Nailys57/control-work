3f0ba1db3cb5a3ec5075af4a7e39b01a768db45d
Nailys57 <nik.rakovich@inbox.ru>
2022-06-15 01:59:04 +0300

    Program

--- control work/Program.cs
+++ control work/Program.cs
@@ -1,2 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using static System.Console;
+Clear();
+
+string[] starray = {"Start", "Flex", "57", "Max", "N", ":-)"};
+int len = starray.Length;
+int y = 0;
+for (int i = 0; i < len; i++)
+{
+    string str = starray[i];
+    int x = str.Length;
+    if (x <= 3)
+    {
+        y++;
+    }
+}
+
+string[] CreateArray(int size) 
+{
+    return new string[size];
+}
+void Fill(int Length, string[] array) 
+{
+    int a = 0;
+    for (int i = 0; i < Length; i++)
+    {
+        string str = starray[i];
+        int x = str.Length;
+        if (x <= 3)
+        {
+            array[a] = str;
+            a++;
+        }
+    }
+}
+string Print(string[] array) 
+{
+    int size = array.Length;
+    string res = string.Empty;
+    res = "[ ";
+    for (int i = 0; i < size; i++)
+    {
+        res += $"{array[i]}";
+        if(i < size - 1)
+        {
+            res += ", ";
+        }
+    }
+    res += " ]";
+    return res;
+}
+
+string[] array = CreateArray(y);
+Fill(len, array);
+WriteLine(Print(array));
+
